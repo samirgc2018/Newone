cf8666b76ad46969a2f797dfe30af7f01a8f3b11
samirgc <samirgc@DESKTOP-V1EFT37>
2018-06-10 02:50:49 +0530

    newupdate

--- App25/App25/App.xaml.cs
+++ App25/App25/App.xaml.cs
@@ -1,6 +1,9 @@
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation (XamlCompilationOptions.Compile)]
 namespace App25
@@ -16,8 +19,8 @@ namespace App25
 
 		protected override void OnStart ()
 		{
-			// Handle when your app starts
-		}
+            AppCenter.Start("android=7b2206cb-5a4e-4825-8822-bffe399fa3c7;" + "uwp={Your UWP App secret here};" + "ios={Your iOS App secret here}", typeof(Analytics), typeof(Crashes));
+        }
 
 		protected override void OnSleep ()
 		{
